c866975812764df1ec6afb2583ad84f1a6773521
usmon <usmonkarimov0726@gmail.com>
2025-02-04 17:21:59 +0500

    used scanner instead of raw reader

--- PrimeTime/main.go
+++ PrimeTime/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -19,8 +19,8 @@ type Resp struct {
 }
 
 type malResp struct {
-	Method string `json:"mithod"`
-	Prime  bool   `json:"prim"`
+	Method string `json:"method"`
+	Prime  bool   `json:"prime"`
 }
 
 const (
@@ -35,53 +35,53 @@ func main() {
 	//start a server
 	listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 
-	// defer listener.Close() //close connection
-
 	if err != nil {
 		fmt.Println("Failed to start a server:", err.Error())
 		os.Exit(1)
 	}
+	defer listener.Close() //close connection
+
 	//Use loop to accept every request to the server
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Failed to connect the client:", conn.RemoteAddr().String(), err.Error())
+			fmt.Println("Failed to connect the client:", err.Error())
+			continue
 		}
 
 		//Concurently hande each request
 		go handleReq(conn)
-
 	}
-
 }
 
 func handleReq(conn net.Conn) {
-	buff := make([]byte, 4096) //create a buffer to hold values
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
 
-	for {
-		n, readErr := conn.Read(buff)
-		if readErr != nil {
-			fmt.Println("Failed to read", readErr.Error())
-			conn.Close()
-		}
-		//Parse and validate Json
-		resp, check := validateJson(buff, n)
-		_, writeErr := conn.Write(resp)
-		if writeErr != nil {
-			fmt.Println("failed to write response", writeErr)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		resp, check := validateJson(line, len(line))
+
+		_, err := conn.Write(append(resp, '\n'))
+		fmt.Println(" response is " + string(resp) + " request was " + string(line[:len(line)]))
+		if err != nil {
+			fmt.Println("failed to write response: ", err)
+			return
 		}
 		if !check {
-			break
+			fmt.Println("request was malformed, terminated connection", conn.RemoteAddr().String())
+			return
 		}
 
 	}
-	defer conn.Close()
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Erorr from reading connection:", err)
+	}
+
 }
 
 func floatToInt(f float64) (int, error) {
-	if f != float64(int(f)) {
-		return 1, errors.New("float is not a whole number")
-	}
+
 	return int(f), nil
 }
 
@@ -98,15 +98,26 @@ func isPrimeNum(n int) bool {
 }
 
 func validateJson(body []byte, n int) ([]byte, bool) {
-	var input Input
+	input := Input{Number: -10100}
+
 	//serialize the json to struct
 	err := json.Unmarshal(body[:n], &input)
+	//return malresp if cannot unmarshal
+
 	if err != nil {
 		fmt.Println("Failed to serailize Json", err)
+		r, _ := json.Marshal(malResp{Method: "error", Prime: false})
+		return r, false
 	}
-	num, _ := floatToInt(input.Number)
 
-	if input.Method == "isPrime" && (num >= 1) {
+	num, err := floatToInt(input.Number)
+	if err != nil {
+		fmt.Println("number is not integer:", err)
+		r, _ := json.Marshal(malResp{Method: "error", Prime: false})
+		return r, false
+	}
+	//check if its correct
+	if input.Method == "isPrime" && input.Number != -10100 {
 		isPrime := isPrimeNum(num)
 		resp := Resp{Method: "isPrime", Prime: isPrime}
 		res, err := json.Marshal(resp)
@@ -115,13 +126,7 @@ func validateJson(body []byte, n int) ([]byte, bool) {
 		}
 		return res, true
 
-	} else {
-		resp := malResp{Method: "Nigga", Prime: false}
-		res, err := json.Marshal(resp)
-		if err != nil {
-			fmt.Println("Failed to Serialize into json", err)
-		}
-		return res, false
 	}
-
+	res, _ := json.Marshal(malResp{Method: "error", Prime: false})
+	return res, false
 }
